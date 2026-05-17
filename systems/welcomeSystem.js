89edb362369236933b3e0c55384971e32b0ba6df
propopopobenipro-dev <propopopobenipro@gmail.com>
2026-05-17 20:59:52 +0200

    Update welcomeSystem.js

--- systems/welcomeSystem.js
+++ systems/welcomeSystem.js
@@ -1,17 +1,32 @@
-const { EmbedBuilder } = require("discord.js");
-const config = require("../config/settings.json");
+const {
+  EmbedBuilder
+} = require("discord.js");
+
+const config =
+  require("../config/settings.json");
 
 module.exports = async (member) => {
 
-  // 👤 AUTO-ROL MIEMBRO
-  const role = member.guild.roles.cache.get(config.memberRoleId);
+  const serverConfig =
+    config.servers[member.guild.id];
+
+  if (!serverConfig) return;
+
+  // 🎭 autorol
+  const role =
+    member.guild.roles.cache.get(
+      serverConfig.memberRoleId
+    );
+
   if (role) {
-    member.roles.add(role).catch(() => {});
+    await member.roles.add(role);
   }
 
+  // 👋 canal bienvenida
   const channel =
     member.guild.channels.cache.find(
-      c => c.name === "🛬│bienvenida"
+      c => c.name ===
+      serverConfig.welcomeChannel
     );
 
   if (!channel) return;
@@ -26,5 +41,7 @@ module.exports = async (member) => {
       member.user.displayAvatarURL()
     );
 
-  channel.send({ embeds: [embed] });
+  channel.send({
+    embeds: [embed]
+  });
 };
