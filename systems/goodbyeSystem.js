9e0d38daece4c0326f13e364691711e8149ef1ba
propopopobenipro-dev <propopopobenipro@gmail.com>
2026-05-17 21:03:56 +0200

    Update goodbyeSystem.js

--- systems/goodbyeSystem.js
+++ systems/goodbyeSystem.js
@@ -1,21 +1,36 @@
-const { EmbedBuilder } = require("discord.js");
+const {
+  EmbedBuilder
+} = require("discord.js");
+
+const config =
+  require("../config/settings.json");
 
 module.exports = async (member) => {
 
-  const channel = member.guild.channels.cache.find(
-    c => c.name === "🛫│despedidas"
-  );
+  const serverConfig =
+    config.servers[member.guild.id];
+
+  if (!serverConfig) return;
+
+  const channel =
+    member.guild.channels.cache.find(
+      c => c.name ===
+      serverConfig.goodbyeChannel
+    );
 
   if (!channel) return;
 
   const embed = new EmbedBuilder()
-    .setTitle("📉 Un ciudadano ha caído")
+    .setTitle("💀 Un jugador salió")
     .setDescription(
-      `⚔️ ${member.user.tag} ha abandonado ImperiumGeo\n\n.`
+      `${member.user.tag} abandonó el servidor`
     )
     .setColor("Red")
-    .setThumbnail(member.user.displayAvatarURL())
-    .setFooter({ text: "Sistema de despedidas automático" });
+    .setThumbnail(
+      member.user.displayAvatarURL()
+    );
 
-  channel.send({ embeds: [embed] });
+  channel.send({
+    embeds: [embed]
+  });
 };
